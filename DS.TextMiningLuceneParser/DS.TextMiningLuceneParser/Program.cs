195866bfd911b6c92418842c9a4dc4816b3cb247
MihaiIonascu <mihai@sense.tech>
2022-10-18 18:36:05 +0300

    Added initial commit

---
+++ DS.TextMiningLuceneParser/DS.TextMiningLuceneParser/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace DS.TextMiningLuceneParser;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("test`");
+    }
+}
